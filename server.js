004a71421dbc55e437ae591b03ed366734f1ea9c
nzixuan <nzixuan@gmail.com>
2024-02-02 15:53:47 +0800

    Set up express and node

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require("express");
+const app = express();
+const port = 3001; // Choose any available port
+
+// Define a simple route
+app.get("/", (req, res) => {
+  res.send("Hello, this is your Express backend!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
