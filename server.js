95e47acd69e9ad025cdad3c4e32ae8674e3a8d50
nzixuan <nzixuan@gmail.com>
2024-02-04 16:34:11 +0800

    Add app closing to end async jest calls

--- server.js
+++ server.js
@@ -3,14 +3,26 @@ const express = require("express");
 const app = express();
 const port = 3001; // Choose any available port
 
+// Start the server
+const server = app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
 // Define a simple route
 app.get("/", (req, res) => {
   res.send("Hello, this is your Express backend!");
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const handleShutdown = () => {
+  console.log("Shutting down gracefully");
+  server.close(() => {
+    console.log("Server closed");
+    process.exit(0);
+  });
+};
 
-module.exports = app; // Export the app for testing
+// Listen for termination signals
+process.on("SIGINT", handleShutdown);
+process.on("SIGTERM", handleShutdown);
+
+module.exports = server;
