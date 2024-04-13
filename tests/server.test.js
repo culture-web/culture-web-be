6216fa4ac5456b81a2bf1682a255d3ac3c3c9c69
nzixuan <nzixuan@gmail.com>
2024-04-13 15:24:18 +0800

    remove extra /

--- tests/server.test.js
+++ tests/server.test.js
@@ -3,7 +3,7 @@ const app = require('../server');
 
 describe('GET /api', () => {
   it('should return status code 200', async () => {
-    const response = await request(app).get('//api');
+    const response = await request(app).get('/api');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('Hello, this is your Express backend!');
   });
