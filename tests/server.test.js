823c671a95162020d46c20ee92f698a408e7b624
nzixuan <nzixuan@gmail.com>
2024-04-13 15:22:25 +0800

    fix server test

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,9 +1,9 @@
 const request = require('supertest');
 const app = require('../server');
 
-describe('GET /', () => {
+describe('GET /api', () => {
   it('should return status code 200', async () => {
-    const response = await request(app).get('/');
+    const response = await request(app).get('//api');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('Hello, this is your Express backend!');
   });
