250cd6e40508be010a2f27963a87d228eb21e0c0
nzixuan <nzixuan@gmail.com>
2024-09-01 22:23:03 +0800

    add face detection to char recognition

--- apiconfig/localApiConfig.js
+++ apiconfig/localApiConfig.js
@@ -1,3 +1,4 @@
 module.exports = {
   kathakaliCharacterClassificationApi: 'http://localhost:5000/classify',
+  facialDetectionApi: 'http://localhost:5001/detect',
 };
--- apiconfig/prodApiConfig.js
+++ apiconfig/prodApiConfig.js
@@ -1,3 +1,4 @@
 module.exports = {
   kathakaliCharacterClassificationApi: 'http://char-recognition:5000/classify',
+  facialDetectionApi: 'http://facial-detection:5000/detect',
 };
