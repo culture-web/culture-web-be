d111e5cddea270fc9af5585b5bb21e624b54159e
Gerald Ng Jun Xiang <geraldngjx@gmail.com>
2025-10-12 18:58:12 +0800

    chore: remove unnecessary comments

--- client/supabaseClient.js
+++ client/supabaseClient.js
@@ -1,13 +1,8 @@
-// supabaseClient.js (Example using Service Role Key for a secure connection)
 const { createClient } = require('@supabase/supabase-js');
 
-// Get environment variables
 const supabaseUrl = process.env.SUPABASE_URL;
-// NOTE: Use the Service Role Key for writing data (cron job) or sensitive server-side operations
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-// Initialize the Supabase client
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-// Endpoint logic will use this 'supabase' instance
 module.exports = supabase;
